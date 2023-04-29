bec02742fb7f7c6e0fb68ca42704850ee72b10d9
OleksiiJS-dev <alexey.kriachok@gmail.com>
2023-04-29 14:13:26 +0300

    actual

--- converter/js/converter.js
+++ converter/js/converter.js
@@ -7,18 +7,12 @@ let giveCurrency = 'USDT';
 let getCurrency = 'USDT';
 let exchangeRateApi = `${giveCurrency}${getCurrency}`;
 let rates = '';
-
-console.log(giveCurrency, getCurrency, exchangeRateApi);   // Присваиваем текстовый контент переменной getCurrency
-
 const giveCurrencyAmount = document.getElementById('input-give-amount');
 const getCurrencyAmount = document.getElementById('input-get-amount');
-
 giveCurrencyAmount.placeholder = 1;
 giveCurrencyAmount.value = giveCurrencyAmount.placeholder;
 getCurrencyAmount.placeholder = 1;
 getCurrencyAmount.value = getCurrencyAmount.placeholder;
-
-//доступные валюты
 const symbols = [
   {
     name: 'USDT',
@@ -61,10 +55,9 @@ const symbols = [
     image: './converter/images/10. USD.svg'
   }
 ];
-
-// добавляем список на страницу html
 for (let i = 0; i < symbols.length; i++) {
   const li = document.createElement('li');
+
   li.value = symbols[i].name
   li.innerHTML = `<span class="converter-give-currency__icon"><img src="${symbols[i].image}" alt="${symbols[i].name}"></span><span class="converter-give-currency__name">${symbols[i].name}</span>`;
   if (li.innerHTML === `<span class="converter-give-currency__icon"><img src="${symbols[i].image}" alt="USDT"></span><span class="converter-give-currency__name">USDT</span>`) {
@@ -72,68 +65,58 @@ for (let i = 0; i < symbols.length; i++) {
   }
 
   giveCurrencyList.appendChild(li.cloneNode(true));
+
   getCurrencyList.appendChild(li.cloneNode(true));
 };
-
-const url = "https://script.google.com/macros/s/AKfycbxvOrKWfSO_lx81ekFy9jAy1V3R-zHH3m1WGHEaSSidfQ8e2YIMAgZTYPqXwjaatWY0YA/exec";
-
+const url = "https://script.google.com/macros/s/AKfycbz2fdEsc0iiTGt1O0pfmuiufeoQJLs4KtckCZGdjBA_10qgUcUqBjgO92tPuEy3lHvi_w/exec";
 fetch(url)
   .then(response => response.json())
   .then(data => rates = data)
   .then(data => console.log(data))
   .catch(error => console.error(error));
-
 const currencyFunctionInput = (a, b) => {
-  const input1 = document.getElementById("input-give-amount")
+  const input1 = document.getElementById("input-give-amount");
   const input2 = document.getElementById('input-get-amount');
-  
-  if (input1.value < 500) {
-    input2.value = input1.value * rates[`${b}`][0];
-  } else if (input1.value < 999) {
-    input2.value = input1.value * rates[`${b}`][1];
-  } else if (input1.value < 4999) {
-    input2.value = input1.value * rates[`${b}`][2];
-  } else if (input1.value < 9999) {
-    input2.value = input1.value * rates[`${b}`][3];
+  if (input1.value < 500) {  
+    input2.value = (input1.value * rates[`${b}`][0]).toFixed(3);
+  } else if (input1.value < 999) {  
+    input2.value = (input1.value * rates[`${b}`][1]).toFixed(3);
+  } else if (input1.value < 4999) {  
+    input2.value = (input1.value * rates[`${b}`][2]).toFixed(3);
+  } else if (input1.value < 9999) {  
+    input2.value = (input1.value * rates[`${b}`][3]).toFixed(3);
   } else if (input1.value >= 10000) {
-    input2.value = input1.value * rates[`${b}`][4];
+    input2.value = (input1.value * rates[`${b}`][4]).toFixed(3);
   }
 }
-
 const currencyFunctionOutput = (a, b) => {
   const input1 = document.getElementById("input-give-amount");
   const input2 = document.getElementById('input-get-amount');
   if (input2.value < 500) {
-    input1.value = input2.value / rates[`${b}`][0];
+    input1.value = (input2.value / rates[`${b}`][0]).toFixed(3);
   } else if (input2.value < 999) {
-    input1.value = input2.value / rates[`${b}`][1];
+    input1.value = (input2.value / rates[`${b}`][1]).toFixed(3);
   } else if (input2.value < 4999) {
-    input1.value = input2.value / rates[`${b}`][2];
+    input1.value = (input2.value / rates[`${b}`][2]).toFixed(3);
   } else if (input2.value < 9999) {
-    input1.value = input2.value / rates[`${b}`][3];
+    input1.value = (input2.value / rates[`${b}`][3]).toFixed(3);
   } else if (input2.value >= 10000) {
-    input1.value = input2.value / rates[`${b}`][4];
+    input1.value = (input2.value / rates[`${b}`][4]).toFixed(3);
   }
 }
-
-// добавление класса :active для элементов списка
 giveCurrencyList.addEventListener("click", function (event) {
-
   if (event.target.tagName === "LI") {
     for (const item of this.children) {
       item.classList.remove("active");
     }
     event.target.classList.add("active");
-    giveCurrency = event.target.textContent
+    giveCurrency = event.target.textContent;
     giveCurrencyLabel.innerText = giveCurrency;
-    exchangeRateApi = `${event.target.textContent}${getCurrency}`
-    console.log(giveCurrency, getCurrency, exchangeRateApi);
+    exchangeRateApi = `${event.target.textContent}${getCurrency}`;
   }
   currencyFunctionInput(rates, exchangeRateApi)
 });
-
 getCurrencyList.addEventListener("click", function (event) {
-
   if (event.target.tagName === "LI") {
     for (const item of this.children) {
       item.classList.remove("active");
@@ -141,40 +124,13 @@ getCurrencyList.addEventListener("click", function (event) {
     event.target.classList.add("active");
     getCurrency = event.target.textContent;
     getCurrencyLabel.innerText = getCurrency;
-    exchangeRateApi = `${giveCurrency}${event.target.textContent}`
-    console.log(giveCurrency, getCurrency, exchangeRateApi);   // Присваиваем текстовый контент переменной getCurrency
+    exchangeRateApi = `${giveCurrency}${event.target.textContent}`;
   }
-  currencyFunctionOutput(rates, exchangeRateApi)
+  currencyFunctionInput(rates, exchangeRateApi);
 });
-
 giveCurrencyAmount.addEventListener('blur', function () {
   currencyFunctionInput(rates, exchangeRateApi);
 });
 getCurrencyAmount.addEventListener('blur', function () {
   currencyFunctionOutput(rates, exchangeRateApi);
 });
-
-
-// else if (event.target.tagName === "SPAN") {
-//   const parentLI = event.target.closest('li');
-//   for (const item of parentLI.parentNode.children) {
-//     item.classList.remove("active");
-//   }
-//   console.log(`вы нажали на ${event.target.tagName}`)
-//   parentLI.classList.add("active");
-//   giveCurrency = event.target.textContent;
-//   giveCurrencyLabel.innerText = giveCurrency
-//   exchangeRateApi = `${event.target.textContent}${getCurrency}`
-//   console.log(event.target.textContent, exchangeRateApi);
-// } else if (event.target.tagName === "IMG") {
-//   const parentLI = event.target.closest('li');
-//   for (const item of parentLI.parentNode.children) {
-//     item.classList.remove("active");
-//   }
-//   console.log(`вы нажали на ${event.target.tagName}`)
-//   parentLI.classList.add("active");
-//   giveCurrency = parentLI.textContent;
-//   giveCurrencyLabel.innerText = giveCurrency
-//   exchangeRateApi = `${parentLI.textContent}${getCurrency}`
-//   console.log(parentLI.textContent, exchangeRateApi);
-// }
---
+++ converter/js/form.js
@@ -0,0 +1,34 @@
+// // 1
+// const formNumberOfApplication = 8888 8888;
+// // 2
+// const formData = new Date();
+// // 3
+// const formReceiveLocation = 'Сайт'
+// // 4
+// const formStatus = 'Новая'
+// // 5 
+// const formName = document
+// // 6
+// const formTel = document
+// // 7
+// const formCard = 
+// // 8
+// const formSell = 
+// // 9
+// const formBuy = 
+// // 10
+// const formSellCurrency = 
+// // 11
+// const formBuyCurrency = 
+// // 12
+// const formCurrencyRate = rates ...
+// // 13
+// const formTg = ''
+// // 14
+// const formMassenger = document
+// // 15
+// const formPromocode = document
+// // 16
+// const formCommentary = document
+
+
